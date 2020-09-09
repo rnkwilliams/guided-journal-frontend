80cc8f8ba0cd469491e1304514c77b9facb0f601
rnkwilliams <rnkwilliams07@gmail.com>
2020-09-09 13:09:25 -0500

    update dynamic forms with bootstrap, add css class for forms

--- src/components/post.js
+++ src/components/post.js
@@ -1,5 +1,6 @@
 class Post {
     constructor(post, postJSON) {
+        debugger
         this.id = post.id
         this.categoryId = postJSON.category_id
         this.date = postJSON.date
@@ -12,13 +13,25 @@ class Post {
 
     renderLi() {
         return `
-        <li data-id=${this.id}>
-            <span>${this.category.name}</span><br>
-            ${this.content1}<br> 
-            ${this.content2}<br> 
-            ${this.content3}<br>
-            <button class="btn" data-id=${this.id}>edit</button>
-        </li><br>`
+        <div data-id=${this.id}>
+            <div class="container">
+                <div class="card mb-4 shadow-sm">   
+                <div class="card-body">
+                    <span>${this.category.name}</span><br><br>
+                    <p class="card-text">
+                    ${this.content1}<br>
+                    ${this.content2}<br>
+                    ${this.content3}<br></p>
+                    <div class="d-flex justify-content-between align-items-center">
+                     <div class="btn-group">
+                        <button type="button" class="btn btn-sm btn-outline-secondary" data-id=${this.id}>Edit</button>
+                     </div>
+                     <small class="text-muted">Category: ${this.category.name}</small>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>`
     }
 
     update({ content1, content2, content3, category_id }) {
@@ -41,57 +54,57 @@ class Post {
     updateSelfCare() {
         return `<form data-id=${this.id} >
         <h3>Edit Journal Entry</h3>
-        <fieldset>
+        <fieldset class="fieldset">
           <legend>I am grateful for: </legend>
-          <textarea name="content1" maxlength="200" rows="4" id="input-content1">${this.content1}</textarea>
+          <textarea class="form-control" rows="4" name="content1" maxlength="200" rows="4" id="input-content1">${this.content1}</textarea>
         </fieldset>
         <fieldset>
           <legend>Daily Affirmation </legend>
-          <textarea name="content2" maxlength="200" rows="4" id="input-content2">${this.content2}</textarea>
+          <textarea class="form-control" rows="4" name="content2" maxlength="200" rows="4" id="input-content2">${this.content2}</textarea>
         </fieldset>
         <fieldset>
           <legend>How could I have made today better? </legend>
-          <textarea name="content3" maxlength="200" rows="4" id="input-content3">${this.content3}</textarea>
+          <textarea class="form-control" rows="4" name="content3" maxlength="200" rows="4" id="input-content3">${this.content3}</textarea>
         </fieldset>
-          <input class="btn" type="submit" value="Edit Entry"></input>
+          <input class="btn btn-secondary mb-2 form" type="submit" value="Edit Entry"></input>
       </form><br>`
     }
 
     updateDailyCare() {
         return `<form data-id=${this.id} >
             <h3>Edit Journal Entry</h3>
-            <fieldset>
+            <fieldset class="fieldset">
             <legend>How was your mental state today? </legend>
-            <textarea name="content1" maxlength="200" rows="4" id="input-content1">${this.content1}</textarea>
+            <textarea class="form-control" rows="4" name="content1" maxlength="200" rows="4" id="input-content1">${this.content1}</textarea>
         </fieldset>
         <fieldset>
             <legend>What did I acccomplish today? </legend>
-            <textarea name="content2" maxlength="200" rows="4" id="input-content2">${this.content2}</textarea>
+            <textarea class="form-control" rows="4" name="content2" maxlength="200" rows="4" id="input-content2">${this.content2}</textarea>
         </fieldset>
         <fieldset>
             <legend>How much did I spend today? </legend>
-            <textarea name="content3" maxlength="200" rows="4" id="input-content3">${this.content3}</textarea>
+            <textarea class="form-control" rows="4" name="content3" maxlength="200" rows="4" id="input-content3">${this.content3}</textarea>
         </fieldset>
-            <input class="btn" type="submit" value="Edit Entry"></input>
+            <input class="btn btn-secondary mb-2 form" type="submit" value="Edit Entry" id="edit"></input>
             </form></br>`
     }
 
     updateGoalSetting() {
         return `<form data-id=${this.id} >
             <h3>Edit Journal Entry</h3>
-            <fieldset>
+            <fieldset class="fieldset">
                         <legend>Today I commit to: </legend>
-                        <textarea name="content1" maxlength="200" rows="4" id="input-content1">${this.content1}</textarea>
+                        <textarea class="form-control" rows="4" name="content1" maxlength="200" rows="4" id="input-content1">${this.content1}</textarea>
                     </fieldset>
                     <fieldset>
                         <legend>Three things I must do today: </legend>
-                        <textarea name="content2" maxlength="200" rows="4" id="input-content2">${this.content2}</textarea>
+                        <textarea class="form-control" rows="4" name="content2" maxlength="200" rows="4" id="input-content2">${this.content2}</textarea>
                     </fieldset>
                     <fieldset>
                         <legend>What is my inspiration for today? </legend>
-                        <textarea name="content3" maxlength="200" rows="4" id="input-content3">${this.content3}</textarea>
+                        <textarea class="form-control" rows="4" name="content3" maxlength="200" rows="4" id="input-content3">${this.content3}</textarea>
                     </fieldset>
-                        <input class="btn" type="submit" value="Edit Entry"></input>`
+                        <input class="btn btn-secondary mb-2 form" type="submit" value="Edit Entry"></input>`
     }
 
     static findById(id) {
@@ -100,50 +113,50 @@ class Post {
 
     static renderSelfCare() {
         return `
-                    <fieldset>
+                    <fieldset class="fieldset">
                         <legend>I am grateful for: </legend>
-                        <textarea name="content1" maxlength="200" rows="4" id="content1"></textarea>
+                        <textarea class="form-control" rows="4" name="content1" maxlength="200" rows="4" id="content1"></textarea>
                     </fieldset >
                     <fieldset>
                         <legend>Daily Affirmation </legend>
-                        <textarea name="content2" maxlength="200" rows="4" id="content2"></textarea>
+                        <textarea class="form-control" rows="4" name="content2" maxlength="200" rows="4" id="content2"></textarea>
                     </fieldset>
                     <fieldset>
                         <legend>How could I have made today better? </legend>
-                        <textarea name="content3" maxlength="200" rows="4" id="content3"></textarea>
+                        <textarea class="form-control" rows="4" name="content3" maxlength="200" rows="4" id="content3"></textarea>
                     </fieldset>
-                        <input class="btn" type="submit" value="Create Entry"></input><br><br>`
+                        <input rows="4" class="btn btn-secondary mb-2 form" type="submit" value="Create Entry"></input><br><br>`
     }
 
     static renderDailyCare() {
-        return `<fieldset>
+        return `<fieldset class="fieldset">
                         <legend>How was your mental state today? </legend>
-                        <textarea name="content1" maxlength="200" rows="4" id="content1"></textarea>
+                        <textarea class="form-control" rows="4" name="content1" maxlength="200" rows="4" id="content1"></textarea>
                     </fieldset>
                     <fieldset>
                         <legend>What did I acccomplish today? </legend>
-                        <textarea name="content2" maxlength="200" rows="4" id="content2"></textarea>
+                        <textarea class="form-control" rows="4" name="content2" maxlength="200" rows="4" id="content2"></textarea>
                     </fieldset>
                     <fieldset>
                         <legend>How much did I spend today? </legend>
-                        <textarea name="content3" maxlength="200" rows="4" id="content3"></textarea>
+                        <textarea class="form-control" rows="4" name="content3" maxlength="200" rows="4" id="content3"></textarea>
                     </fieldset>
-                        <input class="btn" type="submit" value="Create Entry"></input><br><br>`
+                        <input class="btn btn-secondary mb-2 form" type="submit" value="Create Entry"></input><br><br>`
     }
     static renderGoal() {
-        return `<fieldset>
+        return `<fieldset class="fieldset">
                             <legend>Today I commit to: </legend>
-                            <textarea name="content1" maxlength="200" rows="4" id="content1"></textarea>
+                            <textarea class="form-control" rows="4" name="content1" maxlength="200" rows="4" id="content1"></textarea>
                         </fieldset>
                         <fieldset>
                             <legend>Three things I must do today: </legend>
-                            <textarea name="content2" maxlength="200" rows="4" id="content2"></textarea>
+                            <textarea class="form-control" rows="4" name="content2" maxlength="200" rows="4" id="content2"></textarea>
                         </fieldset>
                         <fieldset>
                             <legend>What is my inspiration for today? </legend>
-                            <textarea name="content3" maxlength="200" rows="4" id="content3"></textarea>
+                            <textarea class="form-control" rows="4" name="content3" maxlength="200" rows="4" id="content3"></textarea>
                         </fieldset>
-                            <input class="btn" type="submit" value="Create Entry"></input><br><br>`
+                            <input class="btn btn-secondary mb-2 form" type="submit" value="Create Entry"></input><br><br>`
     }
 
 }
